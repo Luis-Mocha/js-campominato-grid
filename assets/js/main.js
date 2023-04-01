78164fc8d960f5e82e78cb7cfab0f21b6a086669
Luis-Mocha <luismocha97@gmail.com>
2023-04-01 20:47:04 +0200

    input select, tasto play e tasto change difficulty

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,72 @@
 let main = document.querySelector('main');
-let btnEasy = document.querySelector('#btnEasy');
-let btnMedium = document.querySelector('#btnMedium');
-let btnHard = document.querySelector('#btnHard');
+// let btnEasy = document.querySelector('#btnEasy');
+// let btnMedium = document.querySelector('#btnMedium');
+// let btnHard = document.querySelector('#btnHard');
+let form = document.querySelector('form');
+let playDiv = document.querySelector('#playDiv'); 
 
-// creo il div grid
-let grid = myElementFunction('div', 'grid', 'd-flex flex-wrap');
-main.append(grid);
+let changeDiffBtn = myElementFunction('button', 'changeDiffBtn', 'text-uppercase my-3 d-block mx-auto');
+changeDiffBtn.innerHTML = 'Change DIfficulty';
 
 
-/* --- GRIGLIA EASY --- */
+form.addEventListener('submit', function(invioForm) {
+    invioForm.preventDefault();
+
+    // nascondo il tasto play e creo il tasto 'change difficulty'
+    playDiv.classList.add('d-none');
+
+    // let changeDiffBtn = myElementFunction('button', 'changeDiffBtn', 'text-uppercase my-3 d-block mx-auto');
+    // changeDiffBtn.innerHTML = 'Change DIfficulty';
+    this.append(changeDiffBtn);
+
+    // creo il div grid
+    let grid = myElementFunction('div', 'grid', 'd-flex flex-wrap');
+    main.append(grid);
+
+    let difficultyInput = document.querySelector('#difficultyInput').value;
+    console.log(difficultyInput)
+
+    if (difficultyInput == 'easy') {
+        createGrid(100, difficultyInput);
+    }
+    else if (difficultyInput == 'medium') {
+        createGrid(81, difficultyInput);
+    }
+    else {
+        createGrid(49, difficultyInput);
+    }; 
+});
+
+
+// Tasto per refreshare e cambiare difficoltà
+changeDiffBtn.addEventListener('click', function() {
+    alert('Changing difficulty will cause you to lose all progress')
+    location.reload()
+})
+
+
+
+
+
+/*
+// --- GRIGLIA EASY ---
 btnEasy.addEventListener('click', function () {
     createGrid(100, 'easy');
 });
 
 
 
-/* --- GRIGLIA MEDIUM --- */
+// --- GRIGLIA MEDIUM ---
 btnMedium.addEventListener('click', function () {
     createGrid(81, 'medium');
 });
 
 
-/* --- GRIGLIA HARD --- */
+// --- GRIGLIA HARD ---
 btnHard.addEventListener('click', function() {
     createGrid(49, 'hard');
 });
+*/
 
 
 
@@ -82,6 +124,7 @@ function createGrid(numberBoxes, difficulty) {
 
     };
 
+    // creo un div dove inserire le informazioni dei numeri cliccati
     let gridinfo = myElementFunction('div', 'gridInfo', 'd-flex flex-wrap mt-4 row');
     gridinfo.innerHTML = `<h2 class="text-center">Numeri cliccati:</h2>`;
     main.append(gridinfo);
